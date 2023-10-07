016d4b87ea2eaf78a22b52f4da04a7e9d26f7c1d
Marycielo <mberrioz@unal.edu.co>
2023-10-06 21:00:19 -0500

    Update streamlit_igbd_test.py
    Los juegos se muestran en 3 columnas

--- streamlit_igbd_test.py
+++ streamlit_igbd_test.py
@@ -28,13 +28,17 @@ if response.status_code == 200:
     count = 0
 
     # Muestra los juegos en Streamlit
-    for game in games:
-        # Si el juego tiene una imagen de portada y aún no se han mostrado 50 juegos
-        if 'cover' in game and count < 50:
-            st.header(game['name'])
-            image_url = game['cover']['url'].replace('t_thumb', 't_cover_big')
-            image_url = 'https:' + image_url
-            st.image(image_url)
-            
-            # Incrementa el contador
-            count += 1
+    for i in range(0, len(games), 3):  # Cambia el '3' a cuántos juegos quieres por fila
+        cols = st.beta_columns(3)  # Cambia el '3' a cuántos juegos quieres por fila
+
+        for j in range(3):  # Cambia el '3' a cuántos juegos quieres por fila
+            if i + j < len(games) and 'cover' in games[i + j] and count < 50:
+                game = games[i + j]
+                cols[j].header(game['name'])
+                image_url = game['cover']['url'].replace('t_thumb', 't_cover_big')
+                image_url = 'https:' + image_url
+                cols[j].image(image_url)
+                
+                # Incrementa el contador
+                count += 1
+
