5bb17b6b2595b6a50c59c08914c6bb0920e2a730
Marycielo <mberrioz@unal.edu.co>
2023-10-21 19:02:39 -0500

    Update streamlit_igbd_test.py

--- streamlit_igbd_test.py
+++ streamlit_igbd_test.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import requests
 import json
+import streamlit_session_state as SessionState
 
 # Título de la página
 st.title("50 videojuegos que deberías probar")
@@ -26,8 +27,8 @@ def get_games():
 
 games = get_games()
 
-# Inicializa el contador
-count = 0
+# Inicializa el estado de la sesión
+state = SessionState.get(game_id=None)
 
 # Muestra los juegos en Streamlit
 for game in games:
@@ -39,22 +40,22 @@ for game in games:
         st.image(image_url)
         st.write(game['name'])
 
-        # Incrementa el contador
-        count += 1
-
         # Añade un botón para mostrar más detalles
         if st.button(f"Más detalles sobre {game['name']}"):
-            # Realiza una nueva solicitud a la API de IGDB para obtener más detalles sobre el juego
-            body = f'fields name,summary,developers.name,publishers.name,platforms.name; where id = {game["id"]};'
-            response = requests.post(url, headers=headers, data=body)
-            if response.status_code == 200:
-                game_details = json.loads(response.text)[0]
+            state.game_id = game["id"]
 
-                # Muestra los detalles del juego
-                st.write(f"Resumen: {game_details['summary']}")
-                st.write(f"Desarrollador: {', '.join(dev['name'] for dev in game_details['developers'])}")
-                st.write(f"Editor: {', '.join(pub['name'] for pub in game_details['publishers'])}")
-                st.write(f"Plataformas: {', '.join(plat['name'] for plat in game_details['platforms'])}")
+if state.game_id is not None:
+    # Realiza una nueva solicitud a la API de IGDB para obtener más detalles sobre el juego
+    body = f'fields name,summary,developers.name,publishers.name,platforms.name; where id = {state.game_id};'
+    response = requests.post(url, headers=headers, data=body)
+    if response.status_code == 200:
+        game_details = json.loads(response.text)[0]
+
+        # Muestra los detalles del juego
+        st.write(f"Resumen: {game_details['summary']}")
+        st.write(f"Desarrollador: {', '.join(dev['name'] for dev in game_details['developers'])}")
+        st.write(f"Editor: {', '.join(pub['name'] for pub in game_details['publishers'])}")
+        st.write(f"Plataformas: {', '.join(plat['name'] for plat in game_details['platforms'])}")
 
 # Información de los desarrolladores
 developers = [
