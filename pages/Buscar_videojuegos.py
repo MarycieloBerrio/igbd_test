d642d552fcc4fc8693b6b046e7126e108c2f72b7
Marycielo <mberrioz@unal.edu.co>
2023-10-23 02:11:12 -0500

    Update Buscar_videojuegos.py

--- pages/Buscar_videojuegos.py
+++ pages/Buscar_videojuegos.py
@@ -47,8 +47,11 @@ if game_name:
         
         # Muestra la portada del juego en la columna de la izquierda
         if 'cover' in game_info[0] and 'url' in game_info[0]['cover']:
-            col1.image(game_info[0]['cover']['url'], use_column_width=True)
-        
+            image_url = 'https://images.igdb.com/igdb/image/upload/t_cover_big/' + game_info[0]['cover']['url'].split('/')[-1]
+            col1.image(image_url, use_column_width=True)
+        else:
+            st.write("Imagen no disponible")
+
         # Muestra la información del juego en la columna de la derecha
         col2.write(f"**Sinopsis:** {game_info[0]['summary']}" if 'summary' in game_info[0] else "Sinopsis no disponible")
         col2.write(f"**Desarrollador:** {game_info[0]['involved_companies'][0]['company']['name']}" if 'involved_companies' in game_info[0] and game_info[0]['involved_companies'] else "Desarrollador no disponible")
