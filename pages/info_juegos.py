2ff69850c27812ca8fc96a4c7d5dff5c4f6d92df
Marycielo <mberrioz@unal.edu.co>
2023-10-23 00:33:06 -0500

    Update info_juegos.py

--- pages/info_juegos.py
+++ pages/info_juegos.py
@@ -1,3 +1,25 @@
 import streamlit as st
 
-st.write("Hola")
+# Configura el título y el favicon de la página
+st.set_page_config(
+    page_title="Gamer's Companion 🎮",
+    page_icon="🎮",
+)
+
+# Forzar el tema oscuro
+st.markdown("""
+    <style>
+        .stApp {
+            background: #0e1117;
+        }
+
+        div.stTabs button {
+            background: #0e1117;
+        }
+
+        .stApp header {
+            background: #0e1117;
+        }
+        
+    </style>
+    """, unsafe_allow_html=True)
