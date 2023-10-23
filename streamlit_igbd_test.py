5cdfd53fda623154c94e4df199d38b9b6a3bee4e
Marycielo <mberrioz@unal.edu.co>
2023-10-22 21:13:02 -0500

    Update streamlit_igbd_test.py

--- streamlit_igbd_test.py
+++ streamlit_igbd_test.py
@@ -1,6 +1,5 @@
 import streamlit as st
-import requests
-import json
+from back_top50 import obtener_juegos
 
 # Configura el título y el favicon de la página
 st.set_page_config(
@@ -14,27 +13,7 @@ st.markdown("""
         .stApp {
             background: #0e1117;
         }
-
-        div.stTabs button {
-            background: #0e1117;
-        }
-
-        .stApp header {
-            background: #0e1117;
-        }
-        
-        /* ---------- Main page hacks ---------- */
-        
-        section[tabindex=0] div.stButton {
-            left: -999em;
-            position: absolute;
-            /* yeah...this is a very hacky way to send buttons probably to mars */
-        }
-        
-        h3 {
-            color: #756760;
-            margin-bottom: 1rem;
-        }
+        ...
     </style>
     """, unsafe_allow_html=True)
 
@@ -44,56 +23,36 @@ url_imagen = "https://i.imgur.com/qQH31fg.png?1"
 # Añade la imagen como encabezado de la aplicación de Streamlit
 st.image(url_imagen)
 
-# URL de la API de IGDB
-url = "https://api.igdb.com/v4/games"
+# Obtiene los juegos desde el back-end
+games = obtener_juegos()
 
-# Credenciales para la API de IGDB
-headers = {
-    'Client-ID': 'ju1vfy05jqstzoclqv1cs2hsomw1au',
-    'Authorization': 'Bearer 8h1ymcezojqdpcvmz5fvwxal2myoxp',
-}
+# Contador para llevar un registro de cuántos juegos se han mostrado
+count = 0
 
-# Parámetros de la consulta a la API de IGDB
-body = 'fields name,cover.url; limit 100; sort rating desc;\
-        where rating > 70; where rating_count > 1000;'
+# Inicializa la fila HTML con el estilo de borde
+row_html = "<table style='border-color: #fff;'><tr>"
 
-response = requests.post(url, headers=headers, data=body)
+# Muestra los juegos en Streamlit
+for game in games:
+    if 'cover' in game and count < 50:
+        image_url = game['cover']['url'].replace('t_thumb', 't_cover_big')
+        image_url = 'https:' + image_url
 
-# Comprueba si la solicitud fue exitosa
-if response.status_code == 200:
-    # Convierte la respuesta en JSON
-    games = json.loads(response.text)
+        # Incrementa el contador
+        count += 1
 
-    # Contador para llevar un registro de cuántos juegos se han mostrado
-    count = 0
+        # Añade el juego a la fila HTML
+        row_html += f"<td style='border-top: 1px solid #e7e7e7; border-bottom: 1px solid #e7e7e7; \
+                    border-left: 1px solid #0e1117; border-right: 1px solid #0e1117; width: \
+                    100px; height: 200px; text-align: center; vertical-align: top;'><img src='\
+                    {image_url}'style='width: 100px; object-fit: contain;'/><br/><div style=\
+                    'width: 100px; word-wrap: break-word;color: #e7e7e7;'>{game['name']}</div></td>"
 
-    # Inicializa la fila HTML con el estilo de borde
-    row_html = "<table style='border-color: #fff;'><tr>"
-    
-    # Muestra los juegos en Streamlit
-    for game in games:
-        if 'cover' in game and count < 50:
-            image_url = game['cover']['url'].replace('t_thumb', 't_cover_big')
-            image_url = 'https:' + image_url
-                    
-            # Incrementa el contador
-            count += 1
-    
-            # Añade el juego a la fila HTML
-            row_html += f"<td style='border-top: 1px solid #e7e7e7; border-bottom: 1px solid #e7e7e7; \
-                        border-left: 1px solid #0e1117; border-right: 1px solid #0e1117; width: \
-                        100px; height: 200px; text-align: center; vertical-align: top;'><img src='\
-                        {image_url}'style='width: 100px; object-fit: contain;'/><br/><div style=\
-                        'width: 100px; word-wrap: break-word;color: #e7e7e7;'>{game['name']}</div></td>"
-    
-            # Si se han añadido tres juegos a la fila, muestra la fila y comienza una nueva
-            if count % 5 == 0:
-                row_html += "</tr></table>"
-                st.write(row_html, unsafe_allow_html=True)
-                row_html = "<table><tr>"
-
-
-
+        # Si se han añadido tres juegos a la fila, muestra la fila y comienza una nueva
+        if count % 5 == 0:
+            row_html += "</tr></table>"
+            st.write(row_html, unsafe_allow_html=True)
+            row_html = "<table><tr>"
 
 # Información de los desarrolladores
 developers = [
@@ -118,7 +77,6 @@ footer_html += """
     </div>
 </footer>
 """
-
 
 # Agrega un espacio en blanco al final de la página antes del pie de página
 st.write("<br/><br/><br/><br/>", unsafe_allow_html=True)
