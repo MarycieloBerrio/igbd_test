97b5d7c550c9b64f1d1702dc01ad83e51efdb117
Marycielo <mberrioz@unal.edu.co>
2023-10-22 19:59:39 -0500

    Update streamlit_igbd_test.py

--- streamlit_igbd_test.py
+++ streamlit_igbd_test.py
@@ -61,25 +61,26 @@ if response.status_code == 200:
     row_html = "<table style='border-color: #fff;'><tr>"
     
     # Muestra los juegos en Streamlit
-    for game in games:
-        if 'cover' in game and count < 50:
-            image_url = game['cover']['url'].replace('t_thumb', 't_cover_big')
-            image_url = 'https:' + image_url
-                    
-            # Incrementa el contador
-            count += 1
-    
-            # Añade el juego a la fila HTML
-            row_html += f"<td style='border: 1px solid #fff; width: 100px; height: 200px; text-align:\
-                        center; vertical-align: top;'><img src='{image_url}'style='width: 100px;\
-                        object-fit: contain;'/><br/><div style='width: 100px; word-wrap: break-word;\
-                        color: #fff;'>{game['name']}</div></td>"
-    
-            # Si se han añadido tres juegos a la fila, muestra la fila y comienza una nueva
-            if count % 5 == 0:
-                row_html += "</tr></table>"
-                st.write(row_html, unsafe_allow_html=True)
-                row_html = "<table><tr>"
+for game in games:
+    if 'cover' in game and count < 50:
+        image_url = game['cover']['url'].replace('t_thumb', 't_cover_big')
+        image_url = 'https:' + image_url
+                
+        # Incrementa el contador
+        count += 1
+
+        # Añade el juego a la fila HTML
+        row_html += f"<td style='border-top: 1px solid #fff; width: 100px; height: 200px; text-align:\
+                    center; vertical-align: top;'><img src='{image_url}'style='width: 100px;\
+                    object-fit: contain;'/><br/><div style='width: 100px; word-wrap: break-word;\
+                    color: #fff;'>{game['name']}</div></td>"
+
+        # Si se han añadido tres juegos a la fila, muestra la fila y comienza una nueva
+        if count % 5 == 0:
+            row_html += "</tr></table>"
+            st.write(row_html, unsafe_allow_html=True)
+            row_html = "<table><tr>"
+
 
 
 # Información de los desarrolladores
