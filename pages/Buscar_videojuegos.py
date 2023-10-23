42340ccc2e78f46d44a9a9d35670223fcf81dd7f
Marycielo <mberrioz@unal.edu.co>
2023-10-23 02:06:03 -0500

    Update Buscar_videojuegos.py

--- pages/Buscar_videojuegos.py
+++ pages/Buscar_videojuegos.py
@@ -37,10 +37,23 @@ game_name = st.text_input('Busca un videojuego')
 if game_name:
     game_info = get_game_info(game_name)
     
-    # Muestra la información del juego en Streamlit
+    # Verifica si game_info contiene algún elemento
     if game_info:
-        st.write(f"**Nombre:** {game_info[0]['name']}")
-        st.write(f"**Sinopsis:** {game_info[0]['summary']}")
-        st.write(f"**Desarrollador:** {game_info[0]['involved_companies'][0]['company']['name']}")
-        st.write(f"**Plataformas:** {', '.join([platform['name'] for platform in game_info[0]['platforms']])}")
+        # Muestra el nombre del juego en el centro en la parte superior
+        st.write(f"<h1 style='text-align: center;'>{game_info[0]['name']}</h1>" if 'name' in game_info[0] else "Nombre no disponible", unsafe_allow_html=True)
+        
+        # Crea dos columnas para mostrar el cover y la información del juego
+        col1, col2 = st.beta_columns(2)
+        
+        # Muestra la portada del juego en la columna de la izquierda
+        if 'cover' in game_info[0] and 'url' in game_info[0]['cover']:
+            col1.image(game_info[0]['cover']['url'], use_column_width=True)
+        
+        # Muestra la información del juego en la columna de la derecha
+        col2.write(f"**Sinopsis:** {game_info[0]['summary']}" if 'summary' in game_info[0] else "Sinopsis no disponible")
+        col2.write(f"**Desarrollador:** {game_info[0]['involved_companies'][0]['company']['name']}" if 'involved_companies' in game_info[0] and game_info[0]['involved_companies'] else "Desarrollador no disponible")
+        col2.write(f"**Plataformas:** {', '.join([platform['name'] for platform in game_info[0]['platforms']])}" if 'platforms' in game_info[0] and game_info[0]['platforms'] else "Plataformas no disponibles")
+    else:
+        st.write("Lo siento, no pude encontrar ningún juego con ese nombre.")
+
 
