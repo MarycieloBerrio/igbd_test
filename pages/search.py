f329fd992959b1a06c4b159b871d1593a938b1ce
Marycielo <mberrioz@unal.edu.co>
2023-10-23 01:00:34 -0500

    Update search.py

--- pages/search.py
+++ pages/search.py
@@ -1,3 +1,41 @@
+import streamlit as st
+import requests
+
+# Configura el título y el favicon de la página
+st.set_page_config(
+    page_title="Gamer's Companion 🎮",
+    page_icon="🎮",
+)
+
+# Forzar el tema oscuro
+st.markdown("""
+    <style>
+        .stApp {
+            background: #0e1117;
+        }
+
+        div.stTabs button {
+            background: #0e1117;
+        }
+
+        .stApp header {
+            background: #0e1117;
+        }
+        
+    </style>
+    """, unsafe_allow_html=True)
+
+
+# Configura tu clave API de IGDB
+api_key = '8h1ymcezojqdpcvmz5fvwxal2myoxp'
+
+# Define la URL y los encabezados para la solicitud de la API
+url = 'https://api.igdb.com/v4/games'
+headers = {'Client-ID': 'ju1vfy05jqstzoclqv1cs2hsomw1au', 'Authorization': f'Bearer {api_key}'}
+
+# Crea una barra de búsqueda en Streamlit
+game_name = st.text_input('Busca un videojuego')
+
 # Si se introduce un nombre de juego, busca la información del juego
 if game_name:
     # Define la consulta para buscar el juego
