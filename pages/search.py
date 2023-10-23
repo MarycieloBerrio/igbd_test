43aacd264d75b3c0c21221e2bc0f5dccf9332de6
Marycielo <mberrioz@unal.edu.co>
2023-10-23 00:53:07 -0500

    Update search.py

--- pages/search.py
+++ pages/search.py
@@ -29,37 +29,32 @@ st.markdown("""
 # Configura tu clave API de IGDB
 api_key = '8h1ymcezojqdpcvmz5fvwxal2myoxp'
 
-def get_game_info(game_name):
-    # Define la URL y los encabezados para la solicitud de la API
-    url = 'https://api.igdb.com/v4/games'
-    headers = {'Client-ID': 'ju1vfy05jqstzoclqv1cs2hsomw1au', 'Authorization': f'Bearer {api_key}'}
+# Define la URL y los encabezados para la solicitud de la API
+url = 'https://api.igdb.com/v4/games'
+headers = {'Client-ID': 'ju1vfy05jqstzoclqv1cs2hsomw1au', 'Authorization': f'Bearer {api_key}'}
 
-    # Define la consulta para buscar el juego
-    body = f'''
-    fields name, summary, developer, publisher, platforms;
-    where name ~ "{game_name}";'''
+# Define la consulta para buscar el juego
+body = f'''
+fields name, summary, developer, publisher, platforms;
+where name ~ "{game_name}";'''
+
+# Realiza la solicitud a la API
+response = requests.post(url, headers=headers, data=body)
+
+# Devuelve los datos del juego
+return response.json()
+
+# Crea una barra de búsqueda en Streamlit
+game_name = st.text_input('Busca un videojuego')
+
+# Si se introduce un nombre de juego, busca la información del juego
+if game_name:
+    game_info = get_game_info(game_name)
     
-    # Realiza la solicitud a la API
-    response = requests.post(url, headers=headers, data=body)
-    
-    # Devuelve los datos del juego
-    return response.json()
-
-def search_page():
-    # Crea una barra de búsqueda en Streamlit
-    game_name = st.text_input('Busca un videojuego')
-
-    # Si se introduce un nombre de juego, busca la información del juego
-    if game_name:
-        game_info = get_game_info(game_name)
-        
-        # Muestra la información del juego en Streamlit
-        if game_info:
-            st.write(f"**Nombre:** {game_info[0]['name']}")
-            st.write(f"**Sinopsis:** {game_info[0]['summary']}")
-            st.write(f"**Desarrollador:** {game_info[0]['developer']}")
-            st.write(f"**Editor:** {game_info[0]['publisher']}")
-            st.write(f"**Plataformas:** {', '.join(game_info[0]['platforms'])}")
-
-# Asegúrate de llamar a search_page() en tu archivo principal de Streamlit
-
+    # Muestra la información del juego en Streamlit
+    if game_info:
+        st.write(f"**Nombre:** {game_info[0]['name']}")
+        st.write(f"**Sinopsis:** {game_info[0]['summary']}")
+        st.write(f"**Desarrollador:** {game_info[0]['developer']}")
+        st.write(f"**Editor:** {game_info[0]['publisher']}")
+        st.write(f"**Plataformas:** {', '.join(game_info[0]['platforms'])}")
