ec06f49eb92577900f2cb4d2b681eb7e34fe108d
Marycielo <mberrioz@unal.edu.co>
2023-10-23 09:27:16 -0500

    Update Buscar_videojuegos.py

--- pages/Buscar_videojuegos.py
+++ pages/Buscar_videojuegos.py
@@ -56,14 +56,9 @@ if game_name:
         else:
             st.write("Imagen no disponible")
 
-# Muestra la información del juego en la columna de la derecha
-        col2.write(f"**Sinopsis:** {game_info[0]['summary']}"
-                  if 'summary' in game_info[0] else "Sinopsis no disponible")
-        col2.write(f"**Desarrollador:** {game_info[0]['involved_companies'][0]['company']['name']}"
-                  if 'involved_companies' in game_info[0] and game_info[0]['involved_companies']
-                  else "Desarrollador no disponible")
-        col2.write(f"**Plataformas:** {', '.join([platform['name'] for platform in game_info[0]['platforms']])}"
-                  if 'platforms' in game_info[0] and game_info[0]['platforms']
-                  else "Plataformas no disponibles")
+        # Muestra la información del juego en la columna de la derecha
+        col2.write(f"**Sinopsis:** {game_info[0]['summary']}" if 'summary' in game_info[0] else "Sinopsis no disponible")
+        col2.write(f"**Desarrollador:** {game_info[0]['involved_companies'][0]['company']['name']}" if 'involved_companies' in game_info[0] and game_info[0]['involved_companies'] else "Desarrollador no disponible")
+        col2.write(f"**Plataformas:** {', '.join([platform['name'] for platform in game_info[0]['platforms']])}" if 'platforms' in game_info[0] and game_info[0]['platforms'] else "Plataformas no disponibles")
     else:
         st.write("Lo siento, no pude encontrar ningún juego con ese nombre.")
