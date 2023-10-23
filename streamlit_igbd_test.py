7c84cb79bad9e75bf8ef54cbc6f6d0ee4f8a96a0
Marycielo <mberrioz@unal.edu.co>
2023-10-22 21:15:59 -0500

    Update streamlit_igbd_test.py

--- streamlit_igbd_test.py
+++ streamlit_igbd_test.py
@@ -13,7 +13,28 @@ st.markdown("""
         .stApp {
             background: #0e1117;
         }
-        ...
+        
+        div.stTabs button {
+            background: #0e1117;
+        }
+
+        .stApp header {
+            background: #0e1117;
+        }
+        
+        /* ---------- Main page hacks ---------- */
+        
+        section[tabindex=0] div.stButton {
+            left: -999em;
+            position: absolute;
+            /* yeah...this is a very hacky way to send buttons probably to mars */
+        }
+        
+        h3 {
+            color: #756760;
+            margin-bottom: 1rem;
+        }
+        
     </style>
     """, unsafe_allow_html=True)
 
